70ddef4a91d6a234840e9d2ffc4f3e0a5d91db9c
Dee <deepakdilse@gmail.com>
2023-10-23 23:03:59 -0700

    add functionality to create svg from user input

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 console.log('Index.js !');
 
 const fs = require('fs');
+const {Shape, createShape} = require("./lib/shape");
+
 const inquirer = require('inquirer');
 const promptQuestions = [
-    {
-        type: 'input',
-        message: 'Enter upto 3 characters for logo text',
-        name: 'text',
-        default: 'ABC',
-    }
+  {
+    type: "input",
+    message: "Enter upto 3 characters for logo text",
+    name: "text",
+    validate: data => data.length <=3,
+  },
+  {
+    type: "input",
+    message: "Specify a color for the text",
+    name: "textColor",
+    default: "white",
+  },
+  {
+    type: "list",
+    message: "Select a Shape for your logo",
+    name: "shape",
+    default: 'Circle',
+    choices: ["Circle", "Triangle", "Square"],
+  },
+  {
+    type: "input",
+    message: "Specify a background color for the shape",
+    name: "shapeColor",
+    default: "red",
+  },
+
 ];
 
 //  Create a function to write README file
@@ -21,7 +43,25 @@ function writeToFile(fileName, data) {
     });
   }
 
-inquirer.prompt(promptQuestions)
-    .then(answers => {
-        console.log('Answers are: ', answers);
-    });
+
+
+function init() {
+  inquirer.prompt(promptQuestions)
+  .then(answers => {
+      console.log('Answers are: ', answers);
+      // createShape(answers);
+      writeToFile("./examples/logo.svg", createShape(answers));
+      // const testShape = new Shape(answers.shape, answers.shapeColor, answers.text, answers.textColor);
+      // console.log("testShape ", testShape);
+      // console.log(testShape.circle());
+      //call CreateSvg from shape.js and write it to file
+
+
+      // inquirer.prompt(questions).then((answers) => {
+      //   writeToFile("./examples/logo/svg", createSvg(answers));
+      // });
+  });
+}
+
+//start execution
+init();
