39a06b38aba5402927d8f82eb09496b432348692
Dee <deepakdilse@gmail.com>
2023-10-24 20:45:31 -0700

    center svg elements and size adjustments

--- test/shape.test.js
+++ test/shape.test.js
@@ -13,8 +13,8 @@ const mockTriangleSvg =`<svg xmlns="http://www.w3.org/2000/svg" width="300" heig
 </svg>`;
 
 const mockSquareSvg =`<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-<rect x="50" y="0" width="200" height="200" fill="red" />
-<text x="150" y="110" text-anchor="middle" dominant-baseline="middle" fill="black" font-size="60">txt</text>
+<rect x="75" y="25" width="150" height="150" fill="red" />
+<text x="150" y="110" text-anchor="middle" dominant-baseline="middle" fill="black" font-size="35">txt</text>
 </svg>`;
 
 describe('Shape Class', () => {
